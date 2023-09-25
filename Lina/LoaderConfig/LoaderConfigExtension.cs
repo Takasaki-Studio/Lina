1ac883d16f8e30dcdba0312c210fe63c79328bfa
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2023-09-25 13:33:51 -0300

    Add: load configs docs

--- Lina/LoaderConfig/LoaderConfigExtension.cs
+++ Lina/LoaderConfig/LoaderConfigExtension.cs
@@ -3,8 +3,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Lina.LoaderConfig;
 
+/// <summary>
+/// Auto load configs from json file or env variables
+/// </summary>
 public static class LoaderConfigExtension
 {
+    /// <summary>
+    /// Load config from json file or env variable using <a href="https://github.com/aloneguid/config">Config.Net</a> and inject in dependency injection
+    /// </summary>
+    /// <param name="serviceCollection">Service collection</param>
+    /// <param name="jsonFileName">Json file name</param>
+    /// <typeparam name="T">Interface with settings</typeparam>
+    /// <returns>Interface with loaded configurations</returns>
     public static T AddLoaderConfig<T>(this IServiceCollection serviceCollection, string jsonFileName = "config.json")
         where T : class
     {
