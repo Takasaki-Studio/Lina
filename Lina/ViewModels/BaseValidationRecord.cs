102a706a83e563ae63cd074fb0fe287101bf15f8
Takasakiii <lucasmc2709@live.com>
2023-11-10 09:58:34 -0300

    rename BaseViewModel.cs to BaseValidationRecord.cs

--- Lina/ViewModels/BaseValidationRecord.cs
+++ Lina/ViewModels/BaseValidationRecord.cs
@@ -4,15 +4,13 @@ using Lina.Common.Interfaces;
 
 namespace Lina.ViewModels;
 
-
 /// <summary>
 /// Base class to view model with basic validation using <a href="https://docs.fluentvalidation.net/en/latest/">Fluent Validation</a>
 /// </summary>
 /// <typeparam name="TModel">Self ref class</typeparam>
 /// <typeparam name="TValidationClass">Validation class implementation</typeparam>
-[Obsolete("Renamed to BaseValidationRecord")]
-public abstract record BaseViewModel<TModel, TValidationClass> : IValidate, IBaseValidate<TModel, TValidationClass>
-    where TValidationClass : IValidator<TModel>
+public abstract record BaseValidationRecord<TModel, TValidationClass> : IValidate, IBaseValidate<TModel, TValidationClass>
+where TValidationClass : IValidator<TModel>
 {
     /// <summary>
     /// Validate view model with class validation and throw exception if failure
