17fbe76c91bd64658f610676694039f560c283ad
Takasakiii <lucasmc2709@live.com>
2024-02-21 22:20:57 -0300

    update dependencies and add AbstractHostedLifecycleService

---
+++ TakasakiStudio.Lina.AspNet/Workers/AbstractHostedLifecycleService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace TakasakiStudio.Lina.AspNet.Workers;
+
+public abstract class AbstractHostedLifecycleService : IHostedLifecycleService
+{
+    public virtual Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public virtual Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public virtual Task StartedAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public virtual Task StartingAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public virtual Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public virtual Task StoppingAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
