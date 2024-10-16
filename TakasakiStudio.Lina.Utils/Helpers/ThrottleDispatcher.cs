fdbe90557552fa7a1240e2e14fd8ffa4d3d16da9
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2024-10-16 16:16:25 -0300

    Add: debounce and throttle

---
+++ TakasakiStudio.Lina.Utils/Helpers/ThrottleDispatcher.cs
@@ -0,0 +1,39 @@
+﻿namespace TakasakiStudio.Lina.Utils.Helpers;
+
+/// <summary>
+/// Class for manipulate throttle
+/// </summary>
+public static class ThrottleDispatcher
+{
+    /// <summary>
+    /// Throttle
+    /// </summary>
+    /// <param name="action">Function call in throttle</param>
+    /// <param name="interval">Interval for throttle</param>
+    /// <typeparam name="T">Data type</typeparam>
+    /// <returns>Generate function with throttle</returns>
+    public static Action<T> Throttle<T>(Action<T> action, TimeSpan interval)
+    {
+        Task? task = null;
+        var l = new object();
+        T args;
+        return arg =>
+        {
+            args = arg;
+            if (task is not null)
+                return;
+
+            lock (l)
+            {
+                if (task is not null)
+                    return;
+
+                task = Task.Delay(interval).ContinueWith(_ =>
+                {
+                    action(args);
+                    task = null;
+                });
+            }
+        };
+    }
+}
